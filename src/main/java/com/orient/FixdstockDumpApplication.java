32c81b4b3d2ba8ace57e9cd3764abea1560e2503
lipeng <791850806@qq.com>
2019-04-12 09:40:38 +0800

    first commit

---
+++ src/main/java/com/orient/FixdstockDumpApplication.java
@@ -0,0 +1,13 @@
+package com.orient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixdstockDumpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FixdstockDumpApplication.class, args);
+    }
+
+}
